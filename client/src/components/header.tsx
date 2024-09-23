7b87bac806d7469703abd3b5cae507a123d2e9da
louismineo <50332624+louismineo@users.noreply.github.com>
2024-09-23 18:06:56 +0800

    starting with the html now

---
+++ client/src/components/header.tsx
@@ -0,0 +1,13 @@
+export const Header = () =>
+{
+    return(
+    <header>
+    <h1>Employees</h1>
+    
+    <div>
+        <button>Click here</button>
+    </div>
+        
+    </header>
+    )
+}
